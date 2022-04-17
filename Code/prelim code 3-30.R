6a275946d7e2003f8737a4d70bf36c3bf43e476e
rls110 <rls110@duke.edu>
2022-04-17 15:54:51 -0400

    editing and cleaning wrangling code

--- Code/prelim code 3-30.R
+++ Code/prelim code 3-30.R
@@ -1,17 +1,15 @@
-library(tidyverse)
-library(lubridate)
-library(trend)
-library(zoo)
-library(Kendall)
-library(tseries)
+# Setting up library, ggplot theme
+library(tidyverse); library(lubridate); library(trend); library(zoo)
+library(Kendall); library(tseries)
 
 mytheme <- theme_classic(base_size = 14) +
   theme(axis.text = element_text(color = "black"), 
         legend.position = "top")
 theme_set(mytheme)
 
-Durham.1 <- read.csv("tidy_Durham_2010-2014.csv")
-Durham.2 <- read.csv("tidy_Durham_2015-2019.csv")
+# Reading in and wrangling data
+Durham.1 <- read.csv("./Data/Raw/tidy_Durham_2010-2014.csv")
+Durham.2 <- read.csv("./Data/Raw/tidy_Durham_2015-2019.csv")
 
 Durham.1$OBSERVATION.DATE <- as.Date(Durham.1$OBSERVATION.DATE, format = "%m/%d/%Y")
 Durham.1 <- Durham.1 %>%
@@ -33,49 +31,25 @@ spp.counts.2 <- Durham.2 %>%
   summarize(spp_total = length(unique(COMMON.NAME)))
 
 spp.counts.Durham <- rbind(spp.counts.1, spp.counts.2)
+  # total number of species observed in Durham county each month of each year
+ #write.csv(spp.counts.Durham, "./Data/Processed/Durham_spp_richness.csv")
 
-ggplot(spp.counts.Durham) + geom_point(aes(x = MY, y = spp_total))
+ggplot(spp.counts.Durham) + geom_point(aes(x = MY, y = spp_total)) + 
+  labs(x = "Month", y = "Species Richness")
 
+# Time Series Analysis of spp counts in Durham county
 Durham.ts <- ts(spp.counts.Durham$spp_total, start = c(2010,1), frequency = 12)
 Durham.decomp <- stl(Durham.ts, s.window = "periodic")
 plot(Durham.decomp)
 
-# Next step: import SGCN list, get SGCN counts for the different counties
-SGCN <- read.csv("xAPPENDIX-PA1-All-SGCN-by-taxonomic-group-2020-update_FINAL_v3.csv")
-SGCN_birds <- SGCN[c(56:148),c(1:4)]
-colnames(SGCN_birds) <- c("Scientific Name", "Common Name", "Order", "Family")
-write.csv(SGCN_birds, "NC_SGCN_birds.csv")
-SGCN_bird_list <- SGCN_birds$`Common Name`
+Durham.spp.trend <- Kendall::SeasonalMannKendall(Durham.ts) 
+Durham.spp.trend
+summary(Durham.spp.trend)
 
-Durham.total <- rbind(Durham.1, Durham.2)
-Durham.total <- Durham.total %>%
-  mutate(SGCN = ifelse(COMMON.NAME %in% SGCN_bird_list, "Y", "N"))
-
-Durham.SGCN <- unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y"])
-
-Durham.SGCN.by.year <- Durham.total %>%
-  group_by(YEAR) %>%
-  summarize(SGCN_total = length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y"])))
-
-# The not as elegant way since pipe isn't working...
-Durham.SGCN.2010 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2010]))
-Durham.SGCN.2011 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2011]))
-Durham.SGCN.2012 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2012]))
-Durham.SGCN.2013 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2013]))
-Durham.SGCN.2014 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2014]))
-Durham.SGCN.2015 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2015]))
-Durham.SGCN.2016 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2016]))
-Durham.SGCN.2017 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2017]))
-Durham.SGCN.2018 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2018]))
-Durham.SGCN.2019 <- length(unique(Durham.total$COMMON.NAME[Durham.total$SGCN == "Y" & Durham.total$YEAR == 2019]))
-
-Durham.SGCN.by.year <- rbind(Durham.SGCN.2010, Durham.SGCN.2011, Durham.SGCN.2012, 
-                             Durham.SGCN.2013, Durham.SGCN.2014, Durham.SGCN.2015,
-                             Durham.SGCN.2016, Durham.SGCN.2017, Durham.SGCN.2018,
-                             Durham.SGCN.2019)
-Durham.SGCN.by.year <- as.data.frame(Durham.SGCN.by.year) 
-
-Durham.SGCN.by.year <- Durham.SGCN.by.year %>%
-  mutate(YEAR = c(2010:2019))
-
-ggplot(Durham.SGCN.by.year) + geom_point(aes(x = YEAR, y = V1))
+Durham_spp_plot <-
+  ggplot(spp.counts.Durham, aes(x = MY, y = spp_total)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Month/Year", y = "# of Species Observed") +
+  geom_smooth(method = lm)
+print(Durham_spp_plot)
