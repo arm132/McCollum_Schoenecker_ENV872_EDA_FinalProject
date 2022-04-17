fb326225b846735638e5f5b2d14ba59e04a9cecb
rls110 <rls110@duke.edu>
2022-04-17 16:13:48 -0400

    commenting out writing csv lines in script

--- Code/Preliminary Data Wrangling.R
+++ Code/Preliminary Data Wrangling.R
@@ -9,8 +9,8 @@ Durham.1 <- Durham.1 %>%
 Durham.2 <- Durham.2 %>%
   select(COMMON.NAME, OBSERVATION.COUNT, COUNTY, LOCALITY:SAMPLING.EVENT.IDENTIFIER)
 
-write.csv(Durham.1, "./Data/Processed/tidy_Durham_2010-2014.csv")
-write.csv(Durham.2, "./Data/Processed/tidy_Durham_2015-2019.csv")
+# write.csv(Durham.1, "./Data/Processed/tidy_Durham_2010-2014.csv")
+# write.csv(Durham.2, "./Data/Processed/tidy_Durham_2015-2019.csv")
 
 # Wranging eBird Orange County raw data
 Orange.1 <- read.csv("./Data/Raw/eBird_Orange_2010-2014.csv")
@@ -21,8 +21,8 @@ Orange.1 <- Orange.1 %>%
 Orange.2 <- Orange.2 %>%
   select(COMMON.NAME, OBSERVATION.COUNT, COUNTY, LOCALITY:SAMPLING.EVENT.IDENTIFIER)
 
-write.csv(Orange.1, "./Data/Processed/tidy_Orange_2010-2014.csv")
-write.csv(Orange.2, "./Data/Processed/tidy_Orange_2015-2019.csv")
+# write.csv(Orange.1, "./Data/Processed/tidy_Orange_2010-2014.csv")
+# write.csv(Orange.2, "./Data/Processed/tidy_Orange_2015-2019.csv")
 
 # Wranging eBird Wake County raw data
 Wake.1 <- read.csv("./Data/Raw/eBird_Wake_2010-2014.csv")
@@ -33,5 +33,5 @@ Wake.1 <- Wake.1 %>%
 Wake.2 <- Wake.2 %>%
   select(COMMON.NAME, OBSERVATION.COUNT, COUNTY, LOCALITY:SAMPLING.EVENT.IDENTIFIER)
 
-write.csv(Wake.1, "./Data/Processed/tidy_Wake_2010-2014.csv")
-write.csv(Wake.2, "./Data/Processed/tidy_Wake_2015-2019.csv")
+# write.csv(Wake.1, "./Data/Processed/tidy_Wake_2010-2014.csv")
+# write.csv(Wake.2, "./Data/Processed/tidy_Wake_2015-2019.csv")
