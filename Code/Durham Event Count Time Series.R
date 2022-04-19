fcb5fa12fef9d7e4862cd2b98cd6106524ab7b9e
rls110 <rls110@duke.edu>
2022-04-19 17:23:46 -0400

    Adding code for Wake county data wrangling and analysis

--- Code/Durham Event Count Time Series.R
+++ Code/Durham Event Count Time Series.R
@@ -6,6 +6,8 @@ spp.counts.Durham <- read.csv("./Data/Processed/Durham_spp_richness.csv")
 event.counts.Durham <- read.csv("./Data/Processed/Durham_eventcount.csv")
 spp.counts.Orange <- read.csv("./Data/Processed/Orange_spp_richness.csv")
 event.counts.Orange <- read.csv("./Data/Processed/Orange_eventcount.csv")
+spp.counts.Wake <- read.csv("./Data/Processed/Wake_spp_richness.csv")
+event.counts.Wake <- read.csv("./Data/Processed/Wake_eventcount.csv")
 
 # DURHAM TIME SERIES
   # Species richness by month
@@ -60,3 +62,33 @@ plot(Orange.event.decomp)
 Orange.event.trend <- Kendall::SeasonalMannKendall(Orange.event.ts) 
 Orange.event.trend
 summary(Orange.event.trend)
+
+# WAKE TIME SERIES
+  # Species richness by month
+Wake.ts <- ts(spp.counts.Wake$spp_total, start = c(2010,1), frequency = 12)
+Wake.decomp <- stl(Wake.ts, s.window = "periodic")
+plot(Wake.decomp)
+
+Wake.spp.trend <- Kendall::SeasonalMannKendall(Wake.ts) 
+Wake.spp.trend
+summary(Wake.spp.trend)
+
+Wake_spp_plot <-
+  ggplot(spp.counts.Wake, aes(x = MY, y = spp_total)) +
+  geom_point() +
+  geom_line() +
+  labs(x = "Month/Year", y = "# of Species Observed") +
+  geom_smooth(method = lm)
+print(Wake_spp_plot)
+
+# Unique sampling events by month
+Wake.event.ts <- ts(event.counts.Wake$event_total, start = c(2010,1), frequency = 12)
+Wake.event.decomp <- stl(Wake.event.ts, s.window = "periodic")
+plot(Wake.event.decomp)
+
+Wake.event.trend <- Kendall::SeasonalMannKendall(Wake.event.ts) 
+Wake.event.trend
+summary(Wake.event.trend)
+
+
+
--- Code/prelim code 3-30.R
+++ Code/prelim code 3-30.R
@@ -1,10 +1,9 @@
 # Setting up library, ggplot theme
-library(tidyverse); library(lubridate); library(trend); library(zoo)
-library(Kendall); library(tseries)
+library(tidyverse); library(lubridate)
 
 mytheme <- theme_classic(base_size = 14) +
   theme(axis.text = element_text(color = "black"), 
-        legend.position = "top")
+        legend.position = "right")
 theme_set(mytheme)
 
 # Reading in, summarizing, and exploring Durham County data
@@ -51,6 +50,8 @@ event.counts.Durham <- rbind(event.counts.1, event.counts.2)
 
 ggplot(event.counts.Durham) + geom_point(aes(x = MY, y = event_total))
   
+rm(Durham.1, Durham.2, spp.counts.1, spp.counts.2, event.counts.1, event.counts.2)
+
 # Reading in, summarizing, and exploring Orange County data
 Orange.1 <- read.csv("./Data/Processed/tidy_Orange_2010-2014.csv")
 Orange.2 <- read.csv("./Data/Processed/tidy_Orange_2015-2019.csv")
@@ -94,4 +95,84 @@ event.counts.Orange <- rbind(or.event.counts.1, or.event.counts.2)
   # write.csv(event.counts.Orange, "./Data/Processed/Orange_eventcount.csv")
 
 ggplot(event.counts.Orange) + geom_point(aes(x = MY, y = event_total))
+
+rm(Orange.1, Orange.2, or.spp.counts.1, or.spp.counts.2, or.event.counts.1, or.event.counts.2)
+
+# Reading in, summarizing, and exploring Wake County data
+Wake.1 <- read.csv("./Data/Processed/tidy_Wake_2010-2014.csv")
+Wake.2 <- read.csv("./Data/Processed/tidy_Wake_2015-2017.csv")
+Wake.3 <- read.csv("./Data/Processed/tidy_Wake_2018-2019.csv")
+
+Wake.1$OBSERVATION.DATE <- as.Date(Wake.1$OBSERVATION.DATE, format = "%m/%d/%Y")
+Wake.1 <- Wake.1 %>%
+  mutate(MONTH = month(OBSERVATION.DATE)) %>%
+  mutate(YEAR = year(OBSERVATION.DATE)) %>%
+  mutate(MY = my(paste0(MONTH,"-",YEAR)))
+
+Wake.2 <- Wake.2 %>%
+  mutate(MONTH = month(OBSERVATION.DATE)) %>%
+  mutate(YEAR = year(OBSERVATION.DATE)) %>%
+  mutate(MY = my(paste0(MONTH,"-",YEAR)))
+
+Wake.3 <- Wake.3 %>%
+  mutate(MONTH = month(OBSERVATION.DATE)) %>%
+  mutate(YEAR = year(OBSERVATION.DATE)) %>%
+  mutate(MY = my(paste0(MONTH,"-",YEAR)))
+
+wa.spp.counts.1 <- Wake.1 %>%
+  group_by(COUNTY, MY) %>%
+  summarize(spp_total = length(unique(COMMON.NAME)))
+wa.spp.counts.2 <- Wake.2 %>%
+  group_by(COUNTY, MY) %>%
+  summarize(spp_total = length(unique(COMMON.NAME)))
+wa.spp.counts.3 <- Wake.3 %>%
+  group_by(COUNTY, MY) %>%
+  summarize(spp_total = length(unique(COMMON.NAME)))
+
+spp.counts.Wake <- rbind(wa.spp.counts.1, wa.spp.counts.2, wa.spp.counts.3)
+  # total number of species observed in Orange county each month of each year
+  # write.csv(spp.counts.Wake, "./Data/Processed/Wake_spp_richness.csv")
+
+ggplot(spp.counts.Wake) + geom_point(aes(x = MY, y = spp_total)) + 
+  labs(x = "Month", y = "Species Richness")
+
+wa.event.counts.1 <- Wake.1 %>%
+  group_by(COUNTY, MY) %>%
+  summarize(event_total = length(unique(SAMPLING.EVENT.IDENTIFIER)))
+wa.event.counts.2 <- Wake.2 %>%
+  group_by(COUNTY, MY) %>%
+  summarize(event_total = length(unique(SAMPLING.EVENT.IDENTIFIER)))
+wa.event.counts.3 <- Wake.3 %>%
+  group_by(COUNTY, MY) %>%
+  summarize(event_total = length(unique(SAMPLING.EVENT.IDENTIFIER)))
+
+event.counts.Wake <- rbind(wa.event.counts.1, wa.event.counts.2, wa.event.counts.3)
+  # total number of sampling events in Orange county each month of each year--
+  # illustrates changes in the popularity/usage of eBird over time
+  # write.csv(event.counts.Wake, "./Data/Processed/Wake_eventcount.csv")
+
+ggplot(event.counts.Wake) + geom_point(aes(x = MY, y = event_total))
+rm(Wake.1, Wake.2, Wake.3, wa.spp.counts.1, wa.spp.counts.2, wa.spp.counts.3, 
+   wa.event.counts.1, wa.event.counts.2, wa.event.counts.3)
+
+# Aggregating species and event counts for all three counties
+spp.counts <- cbind(spp.counts.Durham[,2:3], spp.counts.Orange[,3], spp.counts.Wake[,3])
+colnames(spp.counts) <- c("MY", "Durham", "Orange", "Wake")
+spp.counts <- pivot_longer(spp.counts, cols = c("Durham", "Orange", "Wake"), names_to = "County")
+  # write.csv(spp.counts, "./Data/Processed/Total_SppRichness.csv")
+
+ggplot(spp.counts) + geom_point(aes(x = MY, y = value, col = County)) + 
+  labs(x = "Month", y = "# of Species", col = "County") # Figure 1
+
+event.counts <- cbind(event.counts.Durham[,2:3], event.counts.Orange[,3], event.counts.Wake[,3])
+colnames(event.counts) <- c("MY", "Durham", "Orange", "Wake")
+event.counts <- pivot_longer(event.counts, cols = c("Durham", "Orange", "Wake"), names_to = "County")
+  # write.csv(event.counts, "./Data/Processed/Total_EventCounts.csv")
+
+ggplot(event.counts) + geom_point(aes(x = MY, y = value, col = County)) + 
+  labs(x = "Month", y = "# of USEs", col = "County") # Figure 2
+
+
+
+
 
