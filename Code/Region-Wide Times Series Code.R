e93b680578ba9c5fb1c7923111eb9dc6ad80e456
rls110 <rls110@duke.edu>
2022-04-20 11:25:19 -0400

    Added second half of data to region-wide time series script

--- Code/Region-Wide Times Series Code.R
+++ Code/Region-Wide Times Series Code.R
@@ -3,8 +3,12 @@ library(tidyverse); library(lubridate); library(trend); library(zoo)
 library(Kendall); library(tseries)
 
 Durham.1 <- read.csv("./Data/Processed/tidy_Durham_2010-2014.csv")
+Durham.2 <- read.csv("./Data/Processed/tidy_Durham_2010-2014.csv")
 Orange.1 <- read.csv("./Data/Processed/tidy_Orange_2010-2014.csv")
+Orange.2 <- read.csv("./Data/Processed/tidy_Orange_2015-2019.csv")
 Wake.1 <- read.csv("./Data/Processed/tidy_Wake_2010-2014.csv")
+Wake.2 <- read.csv("./Data/Processed/tidy_Wake_2015-2017.csv")
+Wake.3 <- read.csv("./Data/Processed/tidy_Wake_2018-2019.csv")
 
 # Extra wrangling for analysis
 total.1 <- rbind(Durham.1, Orange.1, Wake.1)
@@ -14,7 +18,18 @@ total.1 <- total.1 %>%
   mutate(MONTH = month(OBSERVATION.DATE)) %>%
   mutate(YEAR = year(OBSERVATION.DATE)) %>%
   mutate(MY = my(paste0(MONTH,"-",YEAR)))
-spp.counts.total <- total.1 %>%
+
+total.2 <- rbind(Durham.2, Orange.2)
+total.2$OBSERVATION.DATE <- as.Date(total.2$OBSERVATION.DATE, format = "%m/%d/%Y")
+total.2 <- rbind(total.2, Wake.2, Wake.3)
+total.2 <- total.2 %>%
+  mutate(MONTH = month(OBSERVATION.DATE)) %>%
+  mutate(YEAR = year(OBSERVATION.DATE)) %>%
+  mutate(MY = my(paste0(MONTH,"-",YEAR)))
+
+total.data <- rbind(total.1, total.2)
+
+spp.counts.total <- total.data %>%
   group_by(MY) %>%
   summarize(spp_total = length(unique(COMMON.NAME)))
 
